7583c32f49b5a85e15711c4843048aecc0223a4f
gaye <gungorgaye@gmail.com>
2021-12-26 08:49:00 +0300

    Git add yapmadan commite izin vermedi!!!

---
+++ src/main/java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        //I had to do git add
+    }
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,8 @@
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello test");
+        //adding some changes here
+        //make change in local and push
+        //new changesğğuı
+    }
+}
